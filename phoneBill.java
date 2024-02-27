eefc83fe6888c8ff7aec001ef6038c0186e95217
Clayton <cspowell@loyola.edu>
2024-02-27 13:28:45 -0500

    Updated phoneBill.java and Updated algorithm.txt

--- phoneBill.java
+++ phoneBill.java
@@ -1,12 +1,55 @@
-// Read the README.md
-// Read it again
-// Your code here
-// Delete these 4 lines of comments
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
-class HelloWorld {
+public class phoneBill {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Phone Bill Activity!");
-        
+        // Prompt user to enter package name
+        System.out.print("Enter your subscription package (Green/Blue/Purple): ");
+        String packageChoice = scanner.nextLine().toLowerCase();
+
+        // Prompt user to enter data usage
+        System.out.print("Enter the amount of data used (in GB): ");
+        double dataUsage = scanner.nextDouble();
+
+        // Variables to store bill amount and validity of package
+        double billAmount = 0.0;
+        boolean validPackage = true;
+
+        // Calculate bill amount based on package and data usage
+        if (packageChoice.equals("green")) {
+            System.out.print("Do you have a coupon? (yes/no): ");
+            String couponChoice = scanner.next().toLowerCase();
+            
+            double baseCost = 49.99;
+            double includedData = 2.0;
+            double additionalCostPerGB = 15.0;
+            double totalData = includedData + (dataUsage > includedData ? dataUsage - includedData : 0);
+            billAmount = baseCost + (totalData - includedData) * additionalCostPerGB;
+            if (billAmount >= 75 && couponChoice.equals("yes")) {
+                billAmount -= 20;
+            }
+        } else if (packageChoice.equals("blue")) {
+            double baseCost = 70.0;
+            double includedData = 4.0;
+            double additionalCostPerGB = 10.0;
+            double totalData = includedData + (dataUsage > includedData ? dataUsage - includedData : 0);
+            billAmount = baseCost + (totalData - includedData) * additionalCostPerGB;
+        } else if (packageChoice.equals("purple")) {
+            billAmount = 99.95;
+        } else {
+            // Invalid package choice
+            System.out.println("Invalid package choice.");
+            validPackage = false;
+        }
+
+        // Display bill amount if package is valid
+        if (validPackage) {
+            DecimalFormat df = new DecimalFormat("#.##");
+            System.out.println("Your monthly bill amount is: $" + df.format(billAmount));
+        }
+
+        scanner.close();
     }
 }
